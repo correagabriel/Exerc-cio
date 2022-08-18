983b309e3923329adc77fb18b18373d004873417
correagabriel <gabriel.silva2270@etec.sp.gov.br>
2022-08-18 16:49:12 -0300

    upload

---
+++ exercicio/src/newexercicio4.java
@@ -0,0 +1,13 @@
+public class newexercicio4 {
+    public static void main(String[] args) {
+        
+        int fatorialsup = 1;
+
+        for(int n4 = 1; n4 <= 10; n4++) {
+
+            fatorialsup = n4 *  fatorialsup;
+
+            System.out.println("O fatorial de: " + n4 + " = " + fatorialsup);
+        }
+    }
+}
