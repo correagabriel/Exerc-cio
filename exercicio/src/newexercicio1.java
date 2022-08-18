b0cf4fe12a4dd81117ad0b764ede0887a6786e50
correagabriel <gabriel.silva2270@etec.sp.gov.br>
2022-08-18 16:27:24 -0300

    commit

--- exercicio/src/newexercicio1.java
+++ exercicio/src/newexercicio1.java
@@ -2,6 +2,7 @@ public class newexercicio1 {
     public static void main(String[] args) {
         for(int n1 = 150; n1 < 300; n1++) {
             System.out.println(n1);
+            System.out.println("Hello");
         }
     }
 }
