bbf9d4158b26009814fa2e05273fc9d2f7dad781
correagabriel <gabriel.silva2270@etec.sp.gov.br>
2022-08-23 16:52:38 -0300

    comentando

--- exercicio/src/exercicios/registrosdovendedor/salario.java
+++ exercicio/src/exercicios/registrosdovendedor/salario.java
@@ -1,48 +1,48 @@
 package exercicios.registrosdovendedor;
 
 public class salario {
-    String nomeVendedor;
-    String nomeProduto;
-    double valorProduto ;
-    int quantidadeVendida ;
-    double salarioVendedor;
-    double vendas;
-    double pagamento;
-    double vendasSalario;
+    String nomeVendedor; //nome do Vendedor 
+    String nomeProduto; //nome do Produto
+    double valorProduto ; //valor do Produto 
+    int quantidadeVendida ; //quantidade do produto no estoque 
+    double salarioVendedor; //salario do vendedor 
+    double vendas; //quantidade de produtos vendidos 
+    double pagamento; //salario do vendedor (salarioinicila + comissão)
+    double vendasSalario; //valor da comissão 
 
-        public void VendedorNome(String Name){
+        public void VendedorNome(String Name){  //metodo (função) aparecer o nome 
             nomeVendedor = Name;
                 System.out.println("Nome do Vendedor : " + nomeVendedor);
         }
 
-        public void SalarioBase(){
+        public void SalarioBase(){ //função de guardar o salario base
             salarioVendedor = 1500D;
                 System.out.println("Salario Base do Vendedor : " + salarioVendedor);
         }
 
-        public void ProdutosNome(String NomeProduto){
+        public void ProdutosNome(String NomeProduto){ //função guardar o nome do produto
             nomeProduto = NomeProduto;
                 System.out.println("Nome do Produto : " + nomeProduto);
         }
 
-        public void ValorProduto(double Preco){
+        public void ValorProduto(double Preco){ //função guardar o valor do produto
             valorProduto = Preco;
                 System.out.println("Preço Produto : " + valorProduto);
         }
 
-        public void Quantidade(int QuantidadeVendas){
+        public void Quantidade(int QuantidadeVendas){ //função guardar a quantidade  do produto 
             quantidadeVendida = QuantidadeVendas;
                 System.out.println("Quantidade de " + quantidadeVendida);
         }
 
-        public void Vendas(){
+        public void Vendas(){ //função de guarda a quantidade vendida do produto 
             vendas = quantidadeVendida * valorProduto;  
                 System.out.println("Quantidade de Vendas " + nomeProduto + " foi de " + vendas);
         }
 
-        public  void Salario(){
-            vendasSalario = vendas * 0.15;
-            pagamento = salarioVendedor + vendasSalario;
+        public  void Salario(){ //função de guardar o salario 
+            vendasSalario = vendas * 0.15; //comissão 
+            pagamento = salarioVendedor + vendasSalario; //comissão + salarioinicial
                 System.out.println("O valor da comissão foi de : " + vendasSalario);
                 System.out.println("O salario do Vendedor " + nomeVendedor + " foi de : " + pagamento);
         }
--- exercicio/src/exercicios/registrosdovendedor/vendas.java
+++ exercicio/src/exercicios/registrosdovendedor/vendas.java
@@ -2,18 +2,18 @@ package exercicios.registrosdovendedor;
 
 public class vendas {
     public static void main(String[] args) {
-        salario vendas = new salario();
+        salario vendas = new salario(); //primeiro colocar o nome de quem eu vou chamar - nome da onde eu to = novo objeto (mesmo nome de que eu chamei)
 
-        vendas.VendedorNome(" Josuke Higashikawa ");
-        vendas.SalarioBase();
-            System.out.println("");
-        vendas.ProdutosNome(" Nintendo Switch ");
-        vendas.ValorProduto(2369.50D);
-        vendas.Quantidade(10);
-            System.out.println("");
+        vendas.VendedorNome(" Josuke Higashikawa "); //nome do Vendedor - alterei 
+        vendas.SalarioBase(); //seu salario base - não alterei
+            System.out.println(""); //nada 
+        vendas.ProdutosNome(" Nintendo Switch "); //nome do produto que eu alterei 
+        vendas.ValorProduto(2369.50D); //preco do produto que eu criei 
+        vendas.Quantidade(10); //quantidade que eu criei 
+            System.out.println(""); //nada 
 
-        vendas.Vendas();
-            System.out.println("");
-        vendas.Salario();
+        vendas.Vendas(); //quantidade de vendas do produto
+            System.out.println(""); //nada 
+        vendas.Salario(); //salario do vendedor 
     }
 }
